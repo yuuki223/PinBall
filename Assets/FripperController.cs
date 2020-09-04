1f08fc916fc15d4e089a5a9c38647f5d06ba7dcc
yuuki.takahashi <arikitako21@gmail.com>
2020-09-05 00:10:25 +0900

    Lesson5 課題
    発展課題を追加しました。

--- Assets/FripperController.cs
+++ Assets/FripperController.cs
@@ -11,19 +11,26 @@ public class FripperController : MonoBehaviour {
 　　　　　private float defaultAngle = 20;
        //弾いたときの傾き
 　　　　  private float flickAngle = -20;
-　　
+        //左フリッパーを上げた時のfingerId
+        private int leftId;
+        //右フリッパーを上げた時のfingetId
+        private int rightId;
+        
 	// Use this for initialization
 	void Start () {
 	       //HingeJointコンポーネント取得
                this.myHingeJoint = GetComponent<HingeJoint>();
 
                //フリッパーの傾き設定
-               SetAngle(this.defaultAngle);	
+               SetAngle(this.defaultAngle);
+               
+              	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update (){
+　　　　　　　　　　
+
                //左矢印キーを押した時左フリッパーを動かす
                if (Input.GetKeyDown(KeyCode.LeftArrow) && tag == "LeftFripperTag"){
                SetAngle (this.flickAngle);
@@ -39,11 +46,54 @@ public class FripperController : MonoBehaviour {
                }
                if (Input.GetKeyUp(KeyCode.RightArrow) && tag  == "RightFripperTag"){
                SetAngle (this.defaultAngle);
-               } 　	
-	}
-         
+               }
+ 
+                 //画面の左半分をタッチしたら左のフリッパーを上げる
+              for (int i = 0; i <Input.touchCount; i++)                                                   
+              {
+　　　　　　　　　if (Input.touches[i].phase==TouchPhase.Began&&Input.touches[i].position.x<=Screen.width/2 && tag == "LeftFripperTag")
+              {
+                    leftId = Input.touches[i].fingerId;
+            　　　　var pos = Input.touches[i].position;
+              　　 SetAngle (this.flickAngle);
+               　　Debug.LogFormat("左上げ{0} - x:{1}, y:{2}", leftId, pos.x, pos.y);
+              }
+
+               //タッチした指を離したら左フリッパーを下げ
+                 if (Input.touches[i].phase == TouchPhase.Ended && Input.touches[i].position.x <= Screen.width / 2 && tag == "LeftFripperTag")
+                 {
+                    SetAngle(this.defaultAngle);
+                    Debug.Log("左下げ");
+
+                 }
+              }
+　　　　　　　　　//画面右半分をタッチしたら右のフリッパーを上げる
+              for (int i = 0; i <Input.touchCount; i++)
+              {
+              if (Input.touches[i].phase==TouchPhase.Began&&Input.touches[i].position.x>=Screen.width/2&& tag ==  "RightFripperTag")  
+              {
+                  rightId = Input.touches[i].fingerId;
+            　  　var pos = Input.touches[i].position;
+                  SetAngle (this.flickAngle);
+                 Debug.LogFormat("右上げ{0} - x:{1}, y:{2}", rightId, pos.x, pos.y);
+               }
+　　　　　　　　　//タッチした指を離したら右フリッパーを下げる
+                 if( Input.touches[i].phase == TouchPhase.Ended && Input.touches[i].position.x >= Screen.width / 2 && tag == "RightFripperTag")
+                  {    
+                       SetAngle (this.defaultAngle);
+　　　　　　　　　　　　      Debug.Log("右下げ");
+                          
+                   }
+              }
+
+　　　　　　　　
+     　　}
+
+
+          
         //フリッパーの傾き設定
-        public void SetAngle (float angle){
+        public void SetAngle (float angle)
+　　　　　{
                 JointSpring jointSpr = this.myHingeJoint.spring;
                 jointSpr.targetPosition = angle;
                 this.myHingeJoint.spring = jointSpr;
